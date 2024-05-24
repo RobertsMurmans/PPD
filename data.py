fef0e1947a4f029e7e20d8676e227020ba75cfc4
RobertsMurmans <112924947+RobertsMurmans@users.noreply.github.com>
2024-05-24 04:49:11 +0300

    Update data.py

--- data.py
+++ data.py
@@ -4,9 +4,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-host = os.getenv('DB_adress')
+host = os.getenv('DB_Adress')
 name = os.getenv('DB_Name')
-password = os.getenv('DB_password')
+password = os.getenv('DB_Password')
 database = os.getenv('DB')
 
 mydb = mysql.connector.connect(host=host, user=name, password=password, database=database)
